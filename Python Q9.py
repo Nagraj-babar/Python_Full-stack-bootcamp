922678614ff648816ff2fbb93a3e78a1889fb67d
Nagraj-babar <111750928+Nagraj-babar@users.noreply.github.com>
2022-09-19 04:41:49 +0530

    Add files via upload

---
+++ Python Q9.py
@@ -0,0 +1,7 @@
+# Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+name='nagraj'
+age=23
+quali='Eletrical-engineer'
+expe= 0
+print(name,age, quali,expe)
+
---
+++ Python QNo10.py
@@ -0,0 +1,7 @@
+# Write a python script to print values of three variables, each in a new line. Variable contains integer values
+a=19
+b=22
+c=44
+print(a)
+print(b)
+print(c)
